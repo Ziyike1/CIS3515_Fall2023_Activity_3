864d7d2c9da94b267e03fb86040a56a9f8446e27
Ziyike1 <tul51428@temple.edu>
2023-09-21 11:57:15 -0400

    complete the FontSizeAdapter contains 4 get function

--- app/src/main/java/edu/temple/inclassuiacvitivity/FontSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/FontSizeAdapter.kt
@@ -1,24 +1,35 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class FontSizeAdapter (private val fontSize: Array<Int>): BaseAdapter() {
+class FontSizeAdapter (private val context: Context,
+                       private val fontSize: Array<Int>): BaseAdapter() {
     override fun getCount(): Int {
         return fontSize.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return fontSize[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        val textView = TextView(context)
+
+        textView.text = fontSize[position].toString()
+
+        textView.textSize = fontSize[position].toFloat()
+
+        textView.setPadding(5,10,8,10)
+
+        return textView
     }
 
 }
